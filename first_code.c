b9582f696198a618993be66812aa94a77e3b94a4
Rohit Verma <56490258+BeingCoder22@users.noreply.github.com>
2019-10-22 00:46:03 +0530

    Added Make code

---
+++ first_code.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("this is test code\n");
+	printf("added something\n");
+<<<<<<< HEAD
+	printf("added by newc branch\n");
+=======
+	printf("hi all\n");
+>>>>>>> 485a2178da1d3db0e16a5e50f595324037784e0e
+return 0;
+}
